a3f9b791b2076fc1cd0d0af0e01b5c10f7cb195b
Thu Nghiem <richardsnyderyp@gmail.com>
2020-03-19 21:10:47 +0200

    change project card style

--- src/views/Projects/ProjectCard/ProjectCard.js
+++ src/views/Projects/ProjectCard/ProjectCard.js
@@ -13,89 +13,80 @@ const ProjectCard = ({
     <div
       className={classnames([
         styles.ProjectCard,
-        {
-          [styles.ProjectCard_color1]: isWeb
-        },
-        {
-          [styles.ProjectCard_color2]: isDesign
-        },
-        {
-          [styles.ProjectCard_color3]: isGame
-        }
+        { [styles.ProjectCard_imageSpace]: !!imgSrc }
       ])}
     >
-      {/* <div
-        className={classnames(
-          styles.ProjectCard__group,
-          {
-            [styles.ProjectCard__group_color1]: isWeb
-          },
-          {
-            [styles.ProjectCard__group_color2]: isDesign
-          },
-          {
-            [styles.ProjectCard__group_color3]: isGame
-          }
-        )}
-      >
-        {group}
-      </div> */}
-
-      {imgSrc && (
-        <div className={styles.ProjectCard__image}>
-          <img src={imgSrc} alt={projectName} />
-        </div>
-      )}
-
-      <h3 className={styles.ProjectCard__name}>{projectName}</h3>
-
-      <div className={styles.ProjectCard__tagWrapper}>
-        {skills.map(skill => (
-          <span className={styles.ProjectCard__tagWrapper__tag}>{skill}</span>
-        ))}
-      </div>
-
-      <p className={styles.ProjectCard__description}>{description}</p>
-
-      <div className={styles.ProjectCard__footer}>
-        <div>{level}</div>
-      </div>
-
       <div
         className={classnames([
-          styles.ProjectCard__buttonWrapper,
+          styles.ProjectCard__wrapper,
           {
-            [styles.ProjectCard__buttonWrapper_color1]: isWeb
+            [styles.ProjectCard_color1]: isWeb
           },
           {
-            [styles.ProjectCard__buttonWrapper_color2]: isDesign
+            [styles.ProjectCard_color2]: isDesign
           },
           {
-            [styles.ProjectCard__buttonWrapper_color3]: isGame
-          }
+            [styles.ProjectCard_color3]: isGame
+          },
+          { [styles.ProjectCard__wrapper_imageSpace]: !!imgSrc }
         ])}
       >
-        <div className={styles.ProjectCard__buttonWrapper__boxAbove} />
-        <div className={styles.ProjectCard__buttonWrapper__boxBelow} />
-        <button>More</button>
-      </div>
+        {imgSrc && (
+          <div className={styles.ProjectCard__image}>
+            <img src={imgSrc} alt={projectName} />
+          </div>
+        )}
 
-      {/* <div
+        <h3 className={styles.ProjectCard__name}>{projectName}</h3>
+
+        <div className={styles.ProjectCard__tagWrapper}>
+          {skills.map(skill => (
+            <span className={styles.ProjectCard__tagWrapper__tag}>{skill}</span>
+          ))}
+        </div>
+
+        <p className={styles.ProjectCard__description}>{description}</p>
+
+        <div className={styles.ProjectCard__footer}>
+          <div>{level}</div>
+        </div>
+
+        <div
           className={classnames([
-            styles.ProjectCard__progress,
+            styles.ProjectCard__buttonWrapper,
             {
-              [styles.ProjectCard__progress_color1]: isWeb
+              [styles.ProjectCard__buttonWrapper_color1]: isWeb
             },
             {
-              [styles.ProjectCard__progress_color2]: isDesign
+              [styles.ProjectCard__buttonWrapper_color2]: isDesign
             },
             {
-              [styles.ProjectCard__progress_color3]: isGame
+              [styles.ProjectCard__buttonWrapper_color3]: isGame
             }
           ])}
         >
-          <div />
-        </div> */}
+          <div className={styles.ProjectCard__buttonWrapper__boxAbove} />
+          <div className={styles.ProjectCard__buttonWrapper__boxBelow} />
+          <button>More</button>
+        </div>
+
+        {/* <div
+        className={classnames([
+          styles.ProjectCard__progress,
+          {
+            [styles.ProjectCard__progress_color1]: isWeb
+          },
+          {
+            [styles.ProjectCard__progress_color2]: isDesign
+          },
+          {
+            [styles.ProjectCard__progress_color3]: isGame
+          }
+        ])}
+      >
+        <div />
+      </div> */}
+      </div>
     </div>
   );
 };
